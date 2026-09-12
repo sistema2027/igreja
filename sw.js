f47a242b68fcd3e5bf9b73c1f6d909fc2319046e
sistema2027 <priana2023@gmail.com>
2026-09-12 20:58:17 -0300

    sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'financeiro-igreja-v2';
+const CACHE_NAME = 'financeiro-igreja-v3';
 self.addEventListener('install', (e) => {
     e.waitUntil(
         caches.open(CACHE_NAME).then((cache) => cache.addAll([
